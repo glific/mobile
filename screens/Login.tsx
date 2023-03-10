1edb1d6693f2282465bb13a62163a39e61c5b9d3
Kurund Jalmi <kurund.jalmi@gmail.com>
2023-03-10 22:50:44 +0000

    more fixes for login form

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import Button from "../components/ui/Button";
 
 import Input from "../components/ui/Input";
+import { Colors } from "../constants/styles";
 
 const Login = () => {
   const [enteredMobile, setEnteredMobile] = useState("");
   const [enteredPassword, setEnteredPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
 
-  function updateInputValueHandler(inputType, enteredValue) {
+  function updateInputValueHandler(inputType: string, enteredValue: string) {
     switch (inputType) {
       case "mobile":
         setEnteredMobile(enteredValue);
@@ -19,25 +21,41 @@ const Login = () => {
     }
   }
 
+  const onSubmitHandler = () => {};
+
+  let errorDisplay;
+  if (errorMessage) {
+    errorDisplay = <Text style={styles.errorLabel}>{errorMessage}</Text>;
+  }
+
   return (
     <View>
-      <Text>Login Screen</Text>
       <Input
         label="Mobile number"
-        onChangeText={updateInputValueHandler.bind(this, "mobile")}
+        onUpdateValue={updateInputValueHandler.bind(this, "mobile")}
         value={enteredMobile}
         keyboardType="numeric"
         isError={errorMessage ? true : false}
       />
       <Input
         label="Password"
-        onChangeText={updateInputValueHandler.bind(this, "password")}
+        onUpdateValue={updateInputValueHandler.bind(this, "password")}
         secure
         value={enteredPassword}
         isError={errorMessage ? true : false}
       />
+      {errorDisplay}
+      <Button onPress={onSubmitHandler}>
+        <Text>Continue</Text>
+      </Button>
     </View>
   );
 };
 
 export default Login;
+
+const styles = StyleSheet.create({
+  errorLabel: {
+    color: Colors.error100,
+  },
+});
