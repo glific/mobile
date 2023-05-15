fe326eb0aab69c706cb770eec6cd6263fff1a055
Vishal <yadav.40@iitj.ac.in>
2023-05-15 18:46:55 +0530

    Restores use of env varriable for API_BASE_URL

--- config/axios.ts
+++ config/axios.ts
@@ -1,9 +1,9 @@
 import axios, { AxiosInstance } from "axios";
-// import { API_BASE_URL } from "@env"
+import { API_BASE_URL } from "@env"
 
 function createAxiosClient(): AxiosInstance {
   return axios.create({
-    baseURL: "https://api.staging.tides.coloredcow.com/api",//API_BASE_URL,
+    baseURL: API_BASE_URL,
   });
 }
 
