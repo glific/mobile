204020627433f8a5975b5f51ce9abdb01f24c0ae
aman190 <ajaman190@kgpian.iitkgp.ac.in>
2023-05-16 18:22:12 +0530

    Add jest.config.js and Modify package.json

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    preset: 'jest-expo',
+    transformIgnorePatterns: [
+      'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
+    ],
+    collectCoverage: true,
+    collectCoverageFrom: [
+      '**/*.{js,jsx,ts,tsx}',
+      '!**/coverage/**',
+      '!**/node_modules/**',
+      '!**/babel.config.js',
+      '!**/jest.setup.js',
+      '!**/helper.ts',
+      '!**/App.tsx',
+    ],
+    setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+    coverageReporters: ['lcov'],
+};
