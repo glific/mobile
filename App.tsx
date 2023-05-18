6199ff173704d0e2d86f1023647d8a370ed190d4
aman190 <ajaman190@kgpian.iitkgp.ac.in>
2023-05-18 17:54:29 +0530

    Add Drawer and Separate Navigations

--- App.tsx
+++ App.tsx
@@ -1,58 +1,12 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-import { Colors } from "./constants/styles";
-import Login from "./screens/Login";
-import Server from "./screens/Server";
-import Chat from "./screens/Chat";
-
-const Stack = createNativeStackNavigator();
-
-const AuthStack = () => {
-  return (
-    <Stack.Navigator
-      screenOptions={{
-        headerStyle: { backgroundColor: Colors.primary400 },
-        headerTintColor: "white",
-        contentStyle: { backgroundColor: Colors.secondary100 },
-      }}
-    >
-      <Stack.Screen name="Server" component={Server} />
-      <Stack.Screen name="Login" component={Login} />
-      <Stack.Screen name="Chat" component={Chat} />
-    </Stack.Navigator>
-  );
-};
-
-const AuthenticatedStack = () => {
-  return (
-    <Stack.Navigator
-      screenOptions={{
-        headerStyle: { backgroundColor: Colors.primary400 },
-        headerTintColor: "white",
-        contentStyle: { backgroundColor: Colors.secondary100 },
-      }}
-    >
-      <Stack.Screen name="Chat" component={Chat} />
-    </Stack.Navigator>
-  );
-};
-
-const Navigation = () => {
-  return (
-    <NavigationContainer>
-      <AuthStack />
-    </NavigationContainer>
-  );
-};
+import Navigation from "./navigatons";
 
 export default function App() {
   return (
     <>
       <StatusBar style="light" />
-
       <Navigation />
     </>
   );
