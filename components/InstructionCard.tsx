44b5cc36f2f088ee327a05c8901333e394c52256
aman190 <ajaman190@kgpian.iitkgp.ac.in>
2023-05-22 19:47:29 +0530

    Fix Colors import in InstructionCard.tsx

--- components/InstructionCard.tsx
+++ components/InstructionCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import { Colors } from '../../constants/styles'
+import { Colors } from '../constants/styles'
 
 const InstructionCard = () => {
   return (
