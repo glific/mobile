da86e0b94e430de615a3c06f4489b24db85301a5
SoumyadiptoPal <soumyadiptocodes@gmail.com>
2023-05-26 19:52:22 +0530

    Updated Code #55

--- components/NotificationItem.tsx
+++ components/NotificationItem.tsx
@@ -1,67 +1,99 @@
-import React, { useState } from 'react'
-import { View, Text, Image, StyleSheet, Dimensions } from 'react-native'
+import React from 'react';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
 
-type notificationType={
-    header: String,
-    message: String,
-    time: String,
-    type: String
-}
+type notificationType = {
+  header: String;
+  message: String;
+  time: String;
+  type: String;
+};
 const windowWidth = Dimensions.get('window').width;
-const NotificationItem = (props:{notification: notificationType}) => {
-    const {notification}= props;
-
-    const getImage=(type:String)=>{
-        if(type==="Critical")
-        return  require('./images/critical.png');
-        else if(type==="Info")
-        return require('./images/info.png');
-        else
-        return require('./images/warning.png');
-    }
+const NotificationItem = (props: { notification: notificationType }) => {
+  const { notification } = props;
   return (
     <View style={styles.container}>
-        <View style={styles.imgContainer}>
-            <Image
-            source={getImage(notification.type)}
-            style={styles.image}/>
-        </View>
-        <View style={styles.container2}>
+      <View style={styles.imgContainer}>
+        {notification.type === 'Info' ? (
+          <View style={styles.info}>
+            <MaterialCommunityIcons name="message-text-outline" size={24} color="#218AFF" />
+          </View>
+        ) : (
+          <>
+            {notification.type === 'Critical' ? (
+              <View style={styles.critical}>
+                <AntDesign name="setting" size={24} color="#767672" />
+              </View>
+            ) : (
+              <View style={styles.warning}>
+                <AntDesign name="warning" size={24} color="#DD1F1F" />
+              </View>
+            )}
+          </>
+        )}
+      </View>
+      <View style={styles.container2}>
         <Text style={styles.header}>{notification.header}</Text>
         <Text>{notification.message}</Text>
         <Text style={styles.time}>{notification.time}</Text>
-        </View>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default NotificationItem
-const styles= StyleSheet.create({
-    container:{
-        display:"flex",
-        flexDirection:"row",
-        height: 116,
-        borderBottomColor: '#E0E0E0',
-        borderBottomWidth: 1,
-        alignItems:"center"
-    },
-    container2:{
-        width:windowWidth-100
-    },
-    imgContainer:{
-        margin:20
-    },
-    image:{
-        height:48,
-        width:48,
-        borderRadius: 25
-    },
-    header:{
-        fontWeight:"700",
-        color: "#212121"
-    },
-    time:{
-        color:"#6E8E7F",
-        fontWeight:"700"
-    }
-})
+export default NotificationItem;
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    height: 116,
+    borderBottomColor: '#E0E0E0',
+    borderBottomWidth: 1,
+    alignItems: 'center',
+  },
+  container2: {
+    width: windowWidth - 100,
+  },
+  imgContainer: {
+    margin: 20,
+  },
+  info: {
+    height: 48,
+    width: 48,
+    borderRadius: 25,
+    backgroundColor: '#D7E6EF',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderColor: '#218AFF',
+    borderWidth: 1,
+  },
+  warning: {
+    height: 48,
+    width: 48,
+    borderRadius: 25,
+    backgroundColor: '#EFD6D6',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderColor: '#DD1F1F',
+    borderWidth: 1,
+  },
+  critical: {
+    height: 48,
+    width: 48,
+    borderRadius: 25,
+    backgroundColor: '#EAEDEC',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderColor: '#767672',
+    borderWidth: 1,
+  },
+  header: {
+    fontWeight: '700',
+    color: '#212121',
+  },
+  time: {
+    color: '#6E8E7F',
+    fontWeight: '700',
+  },
+});
--- screens/Notifications.tsx
+++ screens/Notifications.tsx
@@ -1,108 +1,104 @@
-import { StyleSheet, Text, View, Dimensions, ScrollView, FlatList } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import React, { useState } from 'react';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import NotificationItem from '../components/NotificationItem';
 
-type notificationType={
-  header: String,
-  message: String,
-  time: String,
-  type: String
-}
+type notificationType = {
+  header: String;
+  message: String;
+  time: String;
+  type: String;
+};
 const Notifications = () => {
-  const Notifications: notificationType[]=[
+  const Notifications: notificationType[] = [
     {
-      header: "Glific stimulator four",
-      message: "Sorry! 24 hrs window closed. Your message cannot be sent at this time.",
-      time: "15 min",
-      type: "Warning"
+      header: 'Glific stimulator four',
+      message: 'Sorry! 24 hrs window closed. Your message cannot be sent at this time.',
+      time: '15 min',
+      type: 'Warning',
     },
     {
-      header: "Glific stimulator four",
-      message: "Sorry! 24 hrs window closed. Your message cannot be sent at this time. Hello How are you? Are you fine",
-      time: "20 min",
-      type: "Warning"
+      header: 'Glific stimulator four',
+      message:
+        'Sorry! 24 hrs window closed. Your message cannot be sent at this time. Hello How are you? Are you fine',
+      time: '20 min',
+      type: 'Warning',
     },
     {
-      header: "Glific stimulator four",
-      message: "Sorry! 24 hrs window closed. Your message cannot be sent at this time.",
-      time: "20 min",
-      type: "Critical"
+      header: 'Glific stimulator four',
+      message: 'Sorry! 24 hrs window closed. Your message cannot be sent at this time.',
+      time: '20 min',
+      type: 'Critical',
     },
     {
-      header: "Glific stimulator four",
-      message: "Sorry! 24 hrs window closed. Your message cannot be sent at this time.",
-      time: "1 hour",
-      type: "Info"
+      header: 'Glific stimulator four',
+      message: 'Sorry! 24 hrs window closed. Your message cannot be sent at this time.',
+      time: '1 hour',
+      type: 'Info',
     },
     {
-      header: "Glific stimulator four",
-      message: "Sorry! 24 hrs window closed. Your message cannot be sent at this time.",
-      time: "1 hour",
-      type: "Info"
-    }
-  ]
+      header: 'Glific stimulator four',
+      message: 'Sorry! 24 hrs window closed. Your message cannot be sent at this time.',
+      time: '1 hour',
+      type: 'Info',
+    },
+  ];
 
-  const[option,setOption]=useState("All");
-  const[notificationArray, setNotificationArray]=useState(Notifications);
-  const pressedAll=()=>{
-    setOption("All");
+  const [option, setOption] = useState('All');
+  const [notificationArray, setNotificationArray] = useState(Notifications);
+  const pressedAll = () => {
+    setOption('All');
     setNotificationArray(Notifications);
-  }
-  const pressedCritical=()=>{
-    setOption("Critical");
-    const filteredArray=filterArrayByField("Critical");
+  };
+  const pressedCritical = () => {
+    setOption('Critical');
+    const filteredArray = filterArrayByField('Critical');
     setNotificationArray(filteredArray);
-  }
-  const pressedWarning=()=>{
-    setOption("Warning");
-    const filteredArray=filterArrayByField("Warning");
+  };
+  const pressedWarning = () => {
+    setOption('Warning');
+    const filteredArray = filterArrayByField('Warning');
     setNotificationArray(filteredArray);
-  }
-  const pressedInfo=()=>{
-    setOption("Info");
-    const filteredArray=filterArrayByField("Info");
+  };
+  const pressedInfo = () => {
+    setOption('Info');
+    const filteredArray = filterArrayByField('Info');
     setNotificationArray(filteredArray);
-  }
-  
+  };
+
   function filterArrayByField(value: String): notificationType[] {
-    return Notifications.filter((item) => item["type"] === value);
+    return Notifications.filter((item) => item['type'] === value);
   }
-
-
 
   return (
     <View style={styles.mainContainer}>
       <View style={styles.navBar}>
-        <View style={(option==="All")?styles.option:{}}>
-        <TouchableOpacity onPress={pressedAll}>
-          <Text style={[styles.text, (option==="All")?styles.active:{} ]}>ALL</Text>
-        </TouchableOpacity>
+        <View style={option === 'All' ? styles.option : {}}>
+          <TouchableOpacity onPress={pressedAll}>
+            <Text style={[styles.text, option === 'All' ? styles.active : {}]}>ALL</Text>
+          </TouchableOpacity>
         </View>
-        <View style={(option==="Critical")?styles.option:{}}>
-        <TouchableOpacity onPress={pressedCritical}>
-          <Text style={[styles.text, (option==="Critical")?styles.active:{}]}>CRITICAL</Text>
-        </TouchableOpacity>
+        <View style={option === 'Critical' ? styles.option : {}}>
+          <TouchableOpacity onPress={pressedCritical}>
+            <Text style={[styles.text, option === 'Critical' ? styles.active : {}]}>CRITICAL</Text>
+          </TouchableOpacity>
         </View>
-        <View style={(option==="Warning")?styles.option:{}}>
-        <TouchableOpacity onPress={pressedWarning}>
-          <Text style={[styles.text, (option==="Warning")?styles.active:{}]}>WARNING</Text>
-        </TouchableOpacity>
+        <View style={option === 'Warning' ? styles.option : {}}>
+          <TouchableOpacity onPress={pressedWarning}>
+            <Text style={[styles.text, option === 'Warning' ? styles.active : {}]}>WARNING</Text>
+          </TouchableOpacity>
         </View>
-        <View style={(option==="Info")?styles.option:{}}>
-        <TouchableOpacity onPress={pressedInfo}>
-          <Text style={[styles.text, (option==="Info")?styles.active:{}]}>INFO</Text>
-        </TouchableOpacity>
+        <View style={option === 'Info' ? styles.option : {}}>
+          <TouchableOpacity onPress={pressedInfo}>
+            <Text style={[styles.text, option === 'Info' ? styles.active : {}]}>INFO</Text>
+          </TouchableOpacity>
         </View>
       </View>
       <ScrollView>
-        {
-          notificationArray.map((item)=>{
-            return <NotificationItem key={notificationArray.indexOf(item)} notification={item} />
-          })
-        }
+        {notificationArray.map((item) => {
+          return <NotificationItem key={notificationArray.indexOf(item)} notification={item} />;
+        })}
       </ScrollView>
-
     </View>
   );
 };
@@ -111,30 +107,30 @@ export default Notifications;
 
 const styles = StyleSheet.create({
   mainContainer: {
-    flex: 1
+    flex: 1,
   },
-  navBar:{
-    display: "flex",
-    flexDirection: "row",
+  navBar: {
+    display: 'flex',
+    flexDirection: 'row',
     height: 59,
-    alignItems: "center",
-    justifyContent: "space-between",
+    alignItems: 'center',
+    justifyContent: 'space-between',
     paddingLeft: 17,
     paddingRight: 90,
-    backgroundColor: "#ECF7F1"
+    backgroundColor: '#ECF7F1',
   },
-  text:{
+  text: {
     fontSize: 14,
-    fontWeight: "700",
-    color: "#6E8E7F",
+    fontWeight: '700',
+    color: '#6E8E7F',
     marginTop: 11,
-    marginBottom: 11
+    marginBottom: 11,
   },
-  active:{
-    color: "#073F24"
+  active: {
+    color: '#073F24',
   },
-  option:{
-    borderBottomWidth:2,
-    borderBottomColor: "#073F24",
-  }
+  option: {
+    borderBottomWidth: 2,
+    borderBottomColor: '#073F24',
+  },
 });
