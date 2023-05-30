055133e988e303f4d8acc937ba1b185239a0ba93
Soumyadipto Pal <119007659+SoumyadiptoPal@users.noreply.github.com>
2023-05-30 20:48:44 +0530

    Updated Notifications.tsx and solved deep scan issues

--- screens/Notifications.tsx
+++ screens/Notifications.tsx
@@ -1,98 +1,80 @@
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
-import React, { useState } from 'react';
+import React, { Dispatch, useState } from 'react';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import NotificationItem from '../components/NotificationItem';
 
 type notificationType = {
-  header: String;
-  message: String;
-  time: String;
-  type: String;
+  header: string;
+  message: string;
+  time: string;
+  type: string;
 };
+const Notification: notificationType[] = [
+  {
+    header: 'Glific stimulator four',
+    message: 'Sorry! 24 hrs window closed. Your message cannot be sent at this time.',
+    time: '15 min',
+    type: 'Warning',
+  },
+  {
+    header: 'Glific stimulator four',
+    message:
+      'Sorry! 24 hrs window closed. Your message cannot be sent at this time. Hello How are you? Are you fine',
+    time: '20 min',
+    type: 'Warning',
+  },
+  {
+    header: 'Glific stimulator four',
+    message: 'Sorry! 24 hrs window closed. Your message cannot be sent at this time.',
+    time: '20 min',
+    type: 'Critical',
+  },
+  {
+    header: 'Glific stimulator four',
+    message: 'Sorry! 24 hrs window closed. Your message cannot be sent at this time.',
+    time: '1 hour',
+    type: 'Info',
+  },
+  {
+    header: 'Glific stimulator four',
+    message: 'Sorry! 24 hrs window closed. Your message cannot be sent at this time.',
+    time: '1 hour',
+    type: 'Info',
+  },
+];
 const Notifications = () => {
-  const Notifications: notificationType[] = [
-    {
-      header: 'Glific stimulator four',
-      message: 'Sorry! 24 hrs window closed. Your message cannot be sent at this time.',
-      time: '15 min',
-      type: 'Warning',
-    },
-    {
-      header: 'Glific stimulator four',
-      message:
-        'Sorry! 24 hrs window closed. Your message cannot be sent at this time. Hello How are you? Are you fine',
-      time: '20 min',
-      type: 'Warning',
-    },
-    {
-      header: 'Glific stimulator four',
-      message: 'Sorry! 24 hrs window closed. Your message cannot be sent at this time.',
-      time: '20 min',
-      type: 'Critical',
-    },
-    {
-      header: 'Glific stimulator four',
-      message: 'Sorry! 24 hrs window closed. Your message cannot be sent at this time.',
-      time: '1 hour',
-      type: 'Info',
-    },
-    {
-      header: 'Glific stimulator four',
-      message: 'Sorry! 24 hrs window closed. Your message cannot be sent at this time.',
-      time: '1 hour',
-      type: 'Info',
-    },
-  ];
-
   const [option, setOption] = useState('All');
-  const [notificationArray, setNotificationArray] = useState(Notifications);
-  const pressedAll = () => {
-    setOption('All');
-    setNotificationArray(Notifications);
-  };
-  const pressedCritical = () => {
-    setOption('Critical');
-    const filteredArray = filterArrayByField('Critical');
-    setNotificationArray(filteredArray);
-  };
-  const pressedWarning = () => {
-    setOption('Warning');
-    const filteredArray = filterArrayByField('Warning');
-    setNotificationArray(filteredArray);
-  };
-  const pressedInfo = () => {
-    setOption('Info');
-    const filteredArray = filterArrayByField('Info');
-    setNotificationArray(filteredArray);
-  };
-
-  function filterArrayByField(value: String): notificationType[] {
-    return Notifications.filter((item) => item['type'] === value);
-  }
+  const [notificationArray, setNotificationArray] = useState(Notification);
 
   return (
     <View style={styles.mainContainer}>
+      <Text style={{display:"none"}}>Notifications</Text>
       <View style={styles.navBar}>
-        <View style={option === 'All' ? styles.option : {}}>
-          <TouchableOpacity onPress={pressedAll}>
-            <Text style={[styles.text, option === 'All' ? styles.active : {}]}>ALL</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={option === 'Critical' ? styles.option : {}}>
-          <TouchableOpacity onPress={pressedCritical}>
-            <Text style={[styles.text, option === 'Critical' ? styles.active : {}]}>CRITICAL</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={option === 'Warning' ? styles.option : {}}>
-          <TouchableOpacity onPress={pressedWarning}>
-            <Text style={[styles.text, option === 'Warning' ? styles.active : {}]}>WARNING</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={option === 'Info' ? styles.option : {}}>
-          <TouchableOpacity onPress={pressedInfo}>
-            <Text style={[styles.text, option === 'Info' ? styles.active : {}]}>INFO</Text>
-          </TouchableOpacity>
-        </View>
+      <RenderOption
+          label="All"
+          selectedOption={option}
+          setNotificationArray={setNotificationArray}
+          setOption={setOption}
+        />
+        <RenderOption
+          label="Critical"
+          selectedOption={option}
+          setNotificationArray={setNotificationArray}
+          setOption={setOption}
+        />
+        <RenderOption
+          label="Warning"
+          selectedOption={option}
+          setNotificationArray={setNotificationArray}
+          setOption={setOption}
+        />
+        <RenderOption
+          label="Info"
+          selectedOption={option}
+          setNotificationArray={setNotificationArray}
+          setOption={setOption}
+
+        />
       </View>
       <ScrollView>
         {notificationArray.map((item) => {
@@ -103,34 +85,70 @@ const Notifications = () => {
   );
 };
 
+const RenderOption = ({
+  label,
+  selectedOption,
+  setNotificationArray,
+  setOption
+}: {
+  label: string,
+  selectedOption: string,
+  setNotificationArray: Dispatch<React.SetStateAction<notificationType[]>>
+setOption: Dispatch<React.SetStateAction<string>>
+}) => {
+  const handleOptionPress = (option: string) => {
+    setOption(option);
+    if (option === 'All') {
+      setNotificationArray(Notification);
+    } else {
+      const filteredArray = filterArrayByField(option);
+      setNotificationArray(filteredArray);
+    }
+  };
+
+  function filterArrayByField(value: string): notificationType[] {
+    return Notification.filter((item) => item['type'] === value);
+  }
+
+  const isActive=(label===selectedOption);
+  return (
+    <View style={isActive ? styles.option : {}}>
+      <TouchableOpacity onPress={()=>handleOptionPress(label)}>
+        <Text style={[styles.text, isActive ? styles.active : {}]}>{label}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
 export default Notifications;
 
+const colors=['#ECF7F1','#6E8E7F','#073F24']
 const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
   },
   navBar: {
+    alignItems: 'center',
+    backgroundColor: colors[0],
     display: 'flex',
     flexDirection: 'row',
     height: 59,
-    alignItems: 'center',
     justifyContent: 'space-between',
     paddingLeft: 17,
     paddingRight: 90,
-    backgroundColor: '#ECF7F1',
   },
   text: {
+    color: colors[1],
     fontSize: 14,
     fontWeight: '700',
-    color: '#6E8E7F',
-    marginTop: 11,
     marginBottom: 11,
+    marginTop: 11,
   },
   active: {
-    color: '#073F24',
+    color: colors[2],
   },
   option: {
+    borderBottomColor: colors[2],
     borderBottomWidth: 2,
-    borderBottomColor: '#073F24',
   },
 });
