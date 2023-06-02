7ef49dfa9699bafef08d026dc2c4471083ff01fd
Abhishek Naula <94128925+Abro0058T@users.noreply.github.com>
2023-06-02 22:28:14 +0530

    Update Chat.test.tsx

--- __tests__/Chat.test.tsx
+++ __tests__/Chat.test.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { fireEvent, render, screen, waitFor } from '@testing-library/react-native';
+import { fireEvent, screen, waitFor } from '@testing-library/react-native';
 import Chat from '../screens/Chat';
 import renderWithAuth from '../utils/authProvider';
 import { GET_CONTACTS } from '../graphql/queries/Contact';
-
-
 
 describe('Contacts screen', () => {
   const mockContacts = {
@@ -74,17 +72,12 @@ describe('Contacts screen', () => {
     expect(mockOnSearchHandler).toBeTruthy();
     expect(mockOnFilter).toBeTruthy();
   });
-  
   test("Should test menu is visible on press or not ",async()=>{
-
-
     const { getByTestId } = renderWithAuth(<Chat />, mocks);
     await waitFor(() => {
       fireEvent.press(getByTestId('menuButton'));
     });
-
-    const menu=screen.queryByTestId("menuId")
+    const menu = screen.queryByTestId("menuId")
     expect(menu).toBeTruthy()
-
-  })
+  });
 });
