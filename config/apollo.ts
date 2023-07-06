4f9c392bb62567d888da568648d7cae40dee660c
aman190 <ajaman190@kgpian.iitkgp.ac.in>
2023-07-06 07:04:53 +0530

    bug: added refresh token functionality
    Signed-off-by: aman190 <ajaman190@kgpian.iitkgp.ac.in>

--- config/apollo.ts
+++ config/apollo.ts
@@ -1,11 +1,61 @@
 import { ApolloClient, InMemoryCache, ApolloLink, HttpLink } from '@apollo/client';
+import { TokenRefreshLink } from 'apollo-link-token-refresh';
+
 import Storage from '../utils/asyncStorage';
+import AxiosService from './axios';
+
+async function checkAuthStatusService() {
+  const sessionValue = await Storage.getData('session');
+  if (sessionValue === null) {
+    return false;
+  }
+
+  const parsedSessionValue = JSON.parse(sessionValue);
+  const tokenExpiryTimeFromSession = parsedSessionValue.token_expiry_time;
+  if (!tokenExpiryTimeFromSession) {
+    return false;
+  }
+  const tokenExpiryTime = new Date(tokenExpiryTimeFromSession);
+  return tokenExpiryTime > new Date();
+}
+
+async function renewAuthToken() {
+  const sessionValue = await Storage.getData('session');
+  if (sessionValue === null) {
+    console.log('No session value');
+  }
+
+  const parsedSessionValue = await JSON.parse(sessionValue);
+  const renewalToken = parsedSessionValue.renewal_token;
+
+  const Client = await AxiosService.createAxiosInstance();
+  try {
+    return Client.post('/v1/session/renew', null, {
+      headers: {
+        Authorization: renewalToken,
+      },
+    });
+  } catch (error) {
+    console.log('Renew token error: ', error);
+  }
+}
 
 // Fetches the uri dynamically
-const customFetch = async (uri, options) => {
+async function customFetch(uri, options) {
   const serverURL = await Storage.getData('serverURL');
   return fetch(serverURL, options);
-};
+}
+
+// Async function to fetch the token
+async function fetchToken() {
+  const sessionValue = await Storage.getData('session');
+  let token = '';
+  if (sessionValue !== null) {
+    const parsedSessionValue = JSON.parse(sessionValue);
+    token = parsedSessionValue.access_token;
+  }
+  return token;
+}
 
 // Create an ApolloLink instance
 const authLink = new ApolloLink((operation, forward) => {
@@ -20,20 +70,27 @@ const authLink = new ApolloLink((operation, forward) => {
   });
 });
 
-// Async function to fetch the token
-async function fetchToken() {
-  const sessionValue = await Storage.getData('session');
-  let token = '';
-  if (sessionValue !== null) {
-    const parsedSessionValue = JSON.parse(sessionValue);
-    token = parsedSessionValue.access_token;
-  }
-  return token;
-}
+const refreshLink: any = new TokenRefreshLink({
+  accessTokenField: 'access_token',
+  isTokenValidOrUndefined: () => checkAuthStatusService(),
+  fetchAccessToken: async () => renewAuthToken(),
+  handleResponse: (_operation, accessTokenField) => async (response: any) => {
+    const tokenResponse: any = [];
+
+    if (response.data) {
+      await Storage.storeData('session', JSON.stringify(response.data.data));
+      tokenResponse[accessTokenField] = response.data.data.access_token;
+    }
+    return tokenResponse;
+  },
+  handleError: (err: Error) => {
+    console.log('Refresh link error: ', err);
+  },
+});
 
 const httpLink = new HttpLink({ fetch: customFetch });
 
 export const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: ApolloLink.from([refreshLink, authLink, httpLink]),
   cache: new InMemoryCache(),
 });
