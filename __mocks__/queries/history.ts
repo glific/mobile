7b576e3141871483dd3be0c79a6e1f73de69fde7
Vishal <yadav.40@iitj.ac.in>
2023-08-08 22:54:26 +0530

    Fixes test

--- __mocks__/queries/history.ts
+++ __mocks__/queries/history.ts
@@ -37,6 +37,10 @@ const createSimilarItem = (id: string) => ({
 for (let i = 3; i <= 10; i++) {
   mockHistory.contactHistory.push(createSimilarItem(i.toString()));
 }
+const moreReponse = { contactHistory: [] };
+for (let i = 11; i <= 12; i++) {
+  moreReponse.contactHistory.push(createSimilarItem(i.toString()));
+}
 export const GET_CONTACT_HISTORY_MOCK = [
   {
     request: {
@@ -51,6 +55,9 @@ export const GET_CONTACT_HISTORY_MOCK = [
     request: {
       query: GET_CONTACT_HISTORY,
       variables: { opts: { order: 'ASC', limit: 10, offset: 11 }, filter: { contactId: '2' } },
+    },
+    result: {
+      data: moreReponse,
     },
   },
 ];
--- __tests__/ContactHistory.test.tsx
+++ __tests__/ContactHistory.test.tsx
@@ -3,7 +3,7 @@ import customRender from '../utils/jestRender';
 
 import ContactHistory from '../screens/ContactHistory';
 import { GET_CONTACT_HISTORY_MOCK, GET_EMPTY_HISTORY_MOCK } from '../__mocks__/queries/history';
-import { waitFor } from '@testing-library/react-native';
+import { fireEvent, waitFor } from '@testing-library/react-native';
 
 describe('Contact History', () => {
   test('renders loading indicator for no data', () => {
@@ -31,4 +31,17 @@ describe('Contact History', () => {
       expect(getByText('Flow Started: FlowName1')).toBeDefined();
     });
   });
+
+  test('renders more history on scroll', async () => {
+    const { getByText, getByTestId, getByLabelText } = customRender(
+      <ContactHistory route={{ params: { id: '2' } }} />,
+      GET_CONTACT_HISTORY_MOCK
+    );
+    await waitFor(() => getByTestId('historyFlatList'));
+    const flatList = getByLabelText('history-list');
+
+    // fire the event to fetch more history
+    flatList.props.onEndReached();
+    expect(getByText('Flow Started: FlowName10')).toBeDefined();
+  }, 5000);
 });
