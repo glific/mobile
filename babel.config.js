9d9acd79050b2fe37a3e57461432e38dd374c1c0
Kurund Jalmi <kurund.jalmi@gmail.com>
2024-03-26 21:07:25 +0000

    error fixes

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
@@ -14,10 +14,7 @@ module.exports = function(api) {
           allowUndefined: true,
         },
       ],
-      [
-        'react-native-reanimated/plugin'
-      ]
+      ['react-native-reanimated/plugin'],
     ],
   };
 };
-
